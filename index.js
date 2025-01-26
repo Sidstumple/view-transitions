0b8d4231aae80bb5b1c737312896ac963a1e9d59
Cyd Stumpel <info@cydstumpel.nl>
2025-01-26 15:01:22 +0100

    add cooler animation option

---
+++ index.js
@@ -0,0 +1,16 @@
+const cooler = document.getElementById('switch');
+const coolerChecked = localStorage.getItem('cooler');
+console.log(coolerChecked);
+
+if (coolerChecked == 'true') {
+  console.log('coolerChecked');
+  cooler.checked = true;
+  document.body.classList.add('cooler');
+}
+
+cooler.addEventListener('change', () => {
+  localStorage.setItem('cooler', cooler.checked);
+  console.log(localStorage.getItem('cooler'));
+  
+  document.body.classList.toggle('cooler', cooler.checked);
+});
